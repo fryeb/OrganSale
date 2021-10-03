67989074ea11b93e0be2b0b14a03741d4b438a1b
Benjamin Frye <ben.j.frye@gmail.com>
2021-10-03 11:52:14 +1100

    Add GameState and Draft Intro Video.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum GameState
+{
+    IntoVideo, // Intro Cutscene
+    Main,
+    DeathVideo,
+    Dead,
+    WinVideo,
+    Win
+}
+
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public GameState state;
     public Config config;
     public PlayerController player;
+
     public List<PlayerController> players = new List<PlayerController>();
 
     void Awake()
